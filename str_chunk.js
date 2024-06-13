9381a36f276bd0171e1d9366cc7476ed8fb3b112
664member <664member@gmail.com>
2024-06-13 17:59:36 +0800

    v0.02

--- str_chunk.js
+++ str_chunk.js
@@ -16,20 +16,45 @@ function getChapterContent() {
   // Пример использования функции
   getChapterContent()
     .then(content => {
-      console.log(str_chunk(content,4000));
+        for(let chunk of str_chunk(content,4000)){
+            console.log(chunk.replace(/\n/g, ''));
+            console.log('\n')
+        }
+      
     })
     .catch(error => {
       console.error(error);
     });
-let str_chunk = (str,cs = 4000)=>{
-    let chunk_size = cs;
-    let res = []
-    res.push(str.slice(0,chunk_size))
-    while(chunk_size<str.length){
-        res.push(str.slice(chunk_size,chunk_size+chunk_size))
-        chunk_size = chunk_size + chunk_size
-    }
-    return res
+
+
+let str_chunk = (str,cs = 4000, beautyText = false)=>{
+        let chunk_size = cs;
+        let res = []
+        if(str.length < cs){return [str]}
+        if(!beautyText){
+                res.push(str.slice(0,chunk_size))
+                while(chunk_size<str.length){
+                    res.push(str.slice(chunk_size,chunk_size+chunk_size))
+                    chunk_size = chunk_size + chunk_size
+                }
+                return res
+        }
+
+
+        
+        let find_index = (str , start, symbol = '.') => {
+            let index;
+            for(let i = start; i < str.length ; i++){
+                    if(str[i] != symbol){
+                            continue
+                    }
+                    index = start + i
+                    break
+            }
+            return index
+        }
+     
+
 }
 
 
